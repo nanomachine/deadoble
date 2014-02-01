2e70379412765c9146e8d2025abfc47dddace387
Stefan Caparros <stefancaparros@gmail.com>
2014-02-01 17:59:46 -0400

    First commit like sinatra rack app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Deadoble::Application.config.secret_token = '78c2e65827295156981efa38dd5ccc7a5825ccc1f8624e80c4556604282b93f78545d87308b13519d3b228d61be92b5216bfa63756221c7606e5362f923b5a1c'
---
+++ deadoble.rb
@@ -0,0 +1,12 @@
+# myapp.rb
+
+require 'rubygems'
+
+require 'sinatra'
+
+get '/' do
+  'Hello world!'
+end
+
+
+#https://api.instagram.com/v1/tags/nofilter?access_token=ACCESS-TOKEN
